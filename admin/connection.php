118c5538e69b46e46fddf4427e0acf6a779080eb
Parul-Srivastava <139477806+Parul-Srivastava@users.noreply.github.com>
2023-10-16 22:50:31 +0530

    admin
    Admin Panel:
    
    Secure login and authentication for administrators.
    Add, edit, and delete books in the library catalog.
    Manage student accounts, including registration and profile updates.
    Track and manage book borrowings and returns.

---
+++ admin/connection.php
@@ -0,0 +1,11 @@
+<?php
+ $db= mysqli_connect("localhost","root","","library");
+
+ if(!$db)
+ {
+ 	die("connection failed: ".mysqli_connect_error());
+ }
+ 	echo "Connection succesful"
+ 
+
+?>
---
+++ admin/index.php
@@ -0,0 +1,87 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Online Library Management System
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<style type="text/css">
+	nav
+	{
+		float: right;
+		word-spacing: 30px;
+		padding: 20px;
+	}
+	nav li 
+	{
+		display: inline-block;
+		line-height: 80px;
+	}
+</style>
+</head>
+
+
+<body>
+	<div class="wrapper">
+		<header>
+		<div class="logo">
+			<img src="images/logo.png">
+		</div>
+
+		<?php
+		if(isset($_SESSION['login_user']))
+		{
+			?>
+				<nav>
+					<ul>
+						<li><a href="index.php">HOME</a></li>
+						<li><a href="books.php">BOOKS</a></li>
+						<li><a href="logout.php">LOGOUT</a></li>
+						<li><a href="feedback.php">FEEDBACK</a></li>
+					</ul>
+				</nav>
+			<?php
+		}
+		else
+		{
+			?>
+						<nav>
+							<ul>
+								<li><a href="index.php">HOME</a></li>
+								<li><a href="books.php">BOOKS</a></li>
+								<li><a href="admin_login.php">LOGIN</a></li>
+								<li><a href="registration.php">SIGN-UP</a></li>
+								<li><a href="feedback.php">FEEDBACK</a></li>
+							</ul>
+						</nav>
+		<?php
+		}
+			
+		?>
+
+			
+		</header>
+		<section>
+		<div class="sec_img">
+			<br><br><br><br>
+	   		<br><br><br><br>
+	   		<h1 style="color:white;text-align: center;text-decoration: none; font-size: 75px;">Welcome to Bibliophile</h1>
+	   		<h1 style="color:white;text-align:center;font-size: 55px;">Open 24/7</h1>
+	   		<h1 style="color:white;text-align:center;font-size: 55px;">Experience the magical world of books!</h1><br>
+		</div>
+	   </section>
+		
+
+	</div>
+	<?php  
+
+		include "footer.php";
+	?>
+</body>
+</html>
